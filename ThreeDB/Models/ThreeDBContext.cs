0fc278d231fa00d066a81cc538ffe44986023335
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-05-20 10:53:31 -0700

    try to get aws elastic beanstalk to work

--- ThreeDB/Models/ThreeDBContext.cs
+++ ThreeDB/Models/ThreeDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using QC = System.Data.SqlClient;
 
 namespace ThreeDB.Models
 {
@@ -12,7 +13,7 @@ namespace ThreeDB.Models
         public DbSet<Data> Datas { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder
-            .UseMySql(@"Server=localhost;Port=8889;database=threedb;uid=root;pwd=root;");
+            .UseMySql(@"Server = tcp:threedbserver.database.windows.net, 1433; Initial Catalog = threedb; Persist Security Info=False;User ID = jasunadmin; Password= ThisPasswordIsFor3db; MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
 
         public ThreeDBContext(DbContextOptions options) : base(options)
         {
